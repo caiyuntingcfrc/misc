f104f1616d6ae4ef9791716986370be2a86a3317
CAI, YUN-TING <caiyuntingcfrc@gmail.com>
2019-01-19 17:33:35 +0800

    rev

--- 106income.R
+++ 106income.R
@@ -1,13 +1,28 @@
 rm(list = ls())
-library(ggmap)
 library(tidyverse)
 
-TC.df <- haven::read_spss(file = "Twin Cities/3.雙城正式_全年級20180704final_han&Lung.sav")
-TC.variables <- labelled::var_label(TC.df)
-class(TC.variables)
+#read fixed width file (temp)
+df <- read_fwf("home/HDATA106", fwf_positions(start = c(1, 4, 9, 79), end = c(8, 8, 78, 80))) 
+colnames(df) <- paste("V", seq(1, 4), sep = "")
+#split by groups (list)
+df_g <- df %>% split(., .[ , "V2"])
+#test with g1
+df_g1 <- as.data.frame(df_g$`00001`)
+test <- spread(df_g1, V4, V3)
+#create a function for splitting by group into a list (tibble forma)
+split_tibble <- function(tibble, col) tibble %>% split(., .[ , col])
+df_g <- split_tibble(df, "X2")
 
-home.df <- read.delim(file = "home/HDATA106", sep = "\t", header = FALSE)
-home.df <- read.table(file = "home/HDATA106", header = FALSE)
-home.df <- read.fwf("home/HDATA106", widths = rep(1, 80), header = FALSE)[1:10, ]
-head(home.df)
+head(home.df); tail(home.df)
 home.df[1]
+
+#tidyr spread gather
+stocks <- data.frame(
+    time = as.Date('2009-01-01') + 0:9,
+    X = rnorm(10, 0, 1),
+    Y = rnorm(10, 0, 2),
+    Z = rnorm(10, 0, 4)
+)
+stocksm <- stocks %>% gather(stock, price, -time)
+stocksm %>% spread(stock, price)
+stocksm %>% spread(time, price)
