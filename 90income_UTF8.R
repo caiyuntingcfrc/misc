e7ed14405812a9c5eb138cbb09923810d86659c9
caiyuntincfrc <caiyuntingfrc@gmail.com>
2019-06-09 17:57:27 +0800

    0608

--- 90income_UTF8.R
+++ 90income_UTF8.R
@@ -1,21 +1,24 @@
 ##### author: CAI YUN-TING ######
-##### The Survey of Family Income and Expenditure, 2000 #####
+##### The Survey of Family Income and Expenditure, 2001 #####
 
 ##### prep and options #####
+# rm
+rm(list = ls())
 # set working directory
 setwd("~/R_wd/tw_inc/")
 # loading packages
 # expss must be loaded after haven
 list.packages <- c("tidyverse", "docxtractr", "readtext", 
-                   "haven", "expss", "microbenchmark", "hablar")
+                   "haven", "hablar")
 # check if the packages are installed
-new.packages <- list.packages[!(list.packages %in% installed.packages()[ , "Package"])]
+l <- !(list.packages %in% installed.packages()[ , "Package"])
+new.packages <- list.packages[l]
 # install new packages
 if(length(new.packages)) install.packages(new.packages)
 # load the packages
 lapply(list.packages, require, character.only = TRUE)
 # remove lists
-rm(list.packages, new.packages)
+rm(l, list.packages, new.packages)
 
 # options
 options(readr.show_progress = TRUE)
@@ -26,74 +29,82 @@ timestamp <- format(Sys.time(), "%m%d-%H%M")
 # processing time
 ptm <- proc.time()
 
+# data source
+path_code <- "AA170026/code90.docx"
+path_dat <- "AA170026/inc90.dat"
+year <- 90
+
 ##### create the codebook ######
 # codebook
 # file is in the default working dirctory
-path_code <- "AA170025/code89.docx" 
-code_tbl_89 <- read_docx(path_code) %>% docx_extract_tbl() %>% .[complete.cases(.), ]
+code_tbl <- read_docx(path_code) %>% 
+        docx_extract_tbl() %>% 
+        .[complete.cases(.), ]
 # add row: card_num
-code_tbl_89 <- rbind(code_tbl_89, c(NA, "card_num", "#/79-80", NA, NA, NA))
+code_tbl <- rbind(code_tbl, c(NA, "card_num", "#/79-80", NA, NA, NA))
 # colnames
-colnames(code_tbl_89) <- c("q_num", "variable", "card_pos", "label", "level", "note")
+colnames(code_tbl) <- c("q_num", "variable", "card_pos", 
+                           "label", "level", "note")
 # variable b1_# - b25_#
-code_tbl_89$`variable`[26:54] <- code_tbl_89$`variable`[26:54] %>% 
+l <- grep("^b", code_tbl$variable)
+code_tbl$variable[l] %<>% 
         str_split("#", simplify = TRUE) %>%
         .[ , 1] %>% as.character()
+rm(l)
+
 # start
-code_tbl_89$`start` <- code_tbl_89$`card_pos` %>% 
+code_tbl$start <- code_tbl$card_pos %>% 
         str_split("/", simplify = TRUE) %>% 
         .[ , 2] %>% str_split("-", simplify = TRUE) %>% 
         .[ ,1] %>% as.integer()
 # end
-code_tbl_89$`end` <- code_tbl_89$`card_pos` %>% 
+code_tbl$end <- code_tbl$card_pos %>% 
         str_split("/", simplify = TRUE) %>% 
         .[ , 2] %>% str_split("-", simplify = TRUE) %>% 
         .[ ,2] %>% as.integer()
-# replace NA in `end`
-code_tbl_89$`end` <- with(code_tbl_89, if_else(is.na(`end`), `start`, `end`))
+# replace NA in end
+code_tbl$end <- with(code_tbl, if_else(is.na(end), start, end))
 
 ##### names of item_xxx ######
-doc.text.parts <- readtext(path_code)$`text` %>% 
+doc.text.parts <- readtext(path_code)$text %>% 
         strsplit("\n") %>% .[[1]]
-# items: [71:819]
-doc.items <- grep("¡G", doc.text.parts, value = TRUE) %>% 
-        .[71:819]
+# items
+doc.items <- grep("ï¼š", doc.text.parts, value = TRUE) %>% 
+        # begin with three digits and character "ï¼š"
+        grep("^[1-9][0-9][0-9]ï¼š", ., value = TRUE)
 # item numbers
-doc.items.part1 <- strsplit(doc.items, "¡G") %>% 
+doc.items.digits <- strsplit(doc.items, "ï¼š") %>% 
         unlist() %>% 
         .[2 * (1:length(doc.text.parts)) -1 ] %>% 
         .[!is.na(.)]
 # item contents
-doc.items.part2 <- strsplit(doc.items, "¡G") %>% 
+doc.items.contents <- strsplit(doc.items, "ï¼š") %>% 
         unlist() %>% 
         .[2 * (1:length(doc.text.parts))] %>% 
         .[!is.na(.)]
 
 ##### data processing and manipulation ######
 # data raw and card_num
-path_dat <- "AA170025/inc89.dat"
 df.source <- read_fwf(path_dat, fwf_positions(start = c(1, 79), 
                                               end = c(80, 80), 
                                               col_names = c("raw", "card_num")
                                               ), 
                       # card_num as integer
-                      col_types = cols(card_num = "i", .default = "c")
-                      ) %>% 
-        # order by card_num
-        .[order(.$`card_num`), ]
+                      col_types = cols(card_num = "i", .default = "c"))
 
 ##### card 01 #####
 # filter out card_num == 1
-x <- filter(df.source, card_num == 1) %>% .[ ,1] %>% .$`raw`
+x <- filter(df.source, card_num == 1) %>% .[ ,1] %>% .$raw
 # define tempfile name and format
 y <- tempfile("tp", fileext = ".dat")
 # write the tempfile
 write(x, file = y)
 # read card 1
-# code_tbl_89[2:25]
-df1 <- read_fwf(y, fwf_positions(code_tbl_89$`start`[2:25], 
-                                 code_tbl_89$`end`[2:25],
-                                 col_names = code_tbl_89$`variable`[2:25]), 
+# code_tbl[2:25]
+l <- grep("x1|area|stage|id|^a", code_tbl$variable)
+df1 <- read_fwf(y, fwf_positions(code_tbl$start[l], 
+                                 code_tbl$end[l],
+                                 col_names = code_tbl$variable[l]), 
                 # define column types (variable classes) in df1
                 col_types = cols(x1 = "c", area = "f", stage = "f", 
                                  id = "c", a1 = "f", a2 = "f", 
@@ -102,11 +113,10 @@ df1 <- read_fwf(y, fwf_positions(code_tbl_89$`start`[2:25],
                                  a8 = "n", a9 = "n", a11 = "f", 
                                  a12 = "n", a13 = "n", a14 = "f", 
                                  a15 = "f", a16 = "f", a17 = "f", 
-                                 a18 = "f", a19 = "n", a20 = "n", a21 = "c")
+                                 a18 = "f", a19 = "n", a20 = "n")
                 ) %>% 
         # order
-        .[order(.$`x1`), ]
-
+        .[order(.$x1), ]
 # free up ram
 gc()
 
@@ -118,22 +128,22 @@ f2 <- function(c, d = c - 1) {
         # then create a temp file wiith "000000001" (for merging data), 
         # which will fill NA.
         # matrix with 29 columns c(2, 26:54)
-        if(c %in% df.source$`card_num`) {
+        if(c %in% df.source$card_num) {
                 # filter out card_num == 02:20 and create a temporary .dat file
-                x <- filter(df.source, card_num == c) %>% .[ ,1] %>% .$`raw`
+                x <- filter(df.source, card_num == c) %>% .[ ,1] %>% .$raw
                 y <- tempfile("tmp", fileext = ".dat")
                 write(x, file = y)
                 # read file [2, 26:54]
-                tmp <- read_fwf(y, fwf_positions(code_tbl_89$`start`[c(2, 26:54)], 
-                                                 code_tbl_89$`end`[c(2, 26:54)])
-                                )
+                l <- grep("^x1|^b", code_tbl$variable)
+                tmp <- read_fwf(y, fwf_positions(code_tbl$start[l], 
+                                                 code_tbl$end[l]))
                 } else {tmp <- matrix(ncol = 30) %>% as_tibble(.name_repair = NULL)
                         tmp[ , 1] <- "00000001"        
                         }
         # name the columns (b1_1, b1_2, b1_3 ......)
         # eg. b1_# , card_num == 2, then # == 1, get b1_1 (#: 1:19)
-        colnames(tmp) <- c("x1", paste(code_tbl_89$`variable`[26:54], d, sep = ""))
-        # tmp$card_num <- as.integer(tmp$`card_num`)
+        l <- grep("^x1|^b", code_tbl$variable)
+        colnames(tmp) <- c("x1", paste(code_tbl$variable[l[-1]], d, sep = ""))
         return(tmp)
         }
 # for loop and left_join (dplyr) 
@@ -159,13 +169,10 @@ df2 <- df2 %>% convert(chr(x1),
                        )
 # b2_, b3_ ... (factor)
 variables <- colnames(df2)
-l <- paste("b", c(1:3, 5:16, 19:20, 22, 24), "_", sep = "") %>% 
-        paste("|", sep = "", collapse = "") %>% paste("b26_", sep = "")
-bb <- grep(l, variables)
+l <- grep("x1|b4|b17|b18|b21|b23|b25|b27|b28|b29", variables)
 # mutate_if
-df2[ , bb] %<>% mutate_if(is.character, as.factor) %>% 
+df2[ , variables[-l]] %<>% mutate_if(is.character, as.factor) %>% 
         mutate_if(is.numeric, as.factor)
-
 # free up ram
 gc()
 
@@ -174,16 +181,16 @@ gc()
 x <- filter(df.source, card_num == 21) %>% .[ ,1] %>% .$raw
 y <- tempfile("tmp", fileext = ".dat")
 write(x, file = y)
-# code_tbl_89[55:85]
-df21 <- read_fwf(y, fwf_positions(code_tbl_89$`start`[c(2, 55:85)], 
-                                  code_tbl_89$`end`[c(2, 55:85)],
+l <- grep("x1|^f", code_tbl$variable)
+df21 <- read_fwf(y, fwf_positions(code_tbl$start[l], 
+                                  code_tbl$end[l],
                                   # variable names
-                                  col_names = code_tbl_89$`variable`[c(2, 55:85)]), 
+                                  col_names = code_tbl$variable[l]), 
                  # define column types
                  cols(x1 = "c", f57 = "f", f58 = "f", .default = "n")
                  ) %>% 
         # order by x1
-        .[order(.$`x1`), ]
+        .[order(.$x1), ]
 
 # free up ram
 gc()
@@ -193,17 +200,18 @@ gc()
 x <- filter(df.source, card_num == 22) %>% .[ ,1] %>% .$raw
 y <- tempfile("tmp", fileext = ".dat")
 write(x, file = y)
-# code_tbl_89[86:112]
-df22 <- read_fwf(y, fwf_positions(code_tbl_89$`start`[c(2, 86:112)], 
-                                  code_tbl_89$`end`[c(2, 86:112)],
+# code_tbl[86:112]
+l <- grep("x1|^c[0-9]", code_tbl$variable)
+df22 <- read_fwf(y, fwf_positions(code_tbl$start[l], 
+                                  code_tbl$end[l],
                                   # variable names
-                                  col_names = code_tbl_89$`variable`[c(2, 86:112)]), 
+                                  col_names = code_tbl$variable[l]), 
                  # define column types
                  col_types = cols(x1 = "c", c15 = "n", c16 = "n",
                                   c17 = "n", c18 = "n", c19 = "n",
                                   c22 = "n", .default = "f")) %>% 
         # order by x1
-        .[order(.$`x1`), ]
+        .[order(.$x1), ]
 
 # free up ram
 gc()
@@ -233,7 +241,7 @@ digi <- c(0:9, 1:9, 0)
 positive <- c(rep("+", 10), rep("-", 10))
 grepbook <- tibble(sym, digi, positive)
 # pattern for grep and gsub
-pattern <- grepbook$`sym` %>% .[2:19]
+pattern <- grepbook$sym %>% .[2:19]
 grepbook$pattern <- c("\\{$", sapply(pattern, paste, "$", sep = ""), "\\}$")
 
 ##### replace symbols with digits #####
@@ -241,13 +249,15 @@ p <- grepbook$pattern
 r <- grepbook$digi
 for(i in 1:10) {
         # postitive [1:10]
-        df23$exp[grep(p[i], df23$exp)] <- gsub(pattern = p[i], 
-                                               replacement = r[i], 
-                                               x = grep(p[i], df23$exp, value = TRUE))
+        a <- grep(p[i], df23$exp)
+        df23$exp[a] <- gsub(pattern = p[i], 
+                            replacement = r[i], 
+                            x = grep(p[i], df23$exp, value = TRUE))
         # negative [11:20]
-        df23$exp[grep(p[i + 10], df23$exp)] <- gsub(pattern = p[i + 10], 
-                                               replacement = r[i + 10], 
-                                               x = grep(p[i + 10], df23$exp, value = TRUE)) %>% 
+        b <- grep(p[i + 10], df23$exp)
+        df23$exp[b] <- gsub(pattern = p[i + 10], 
+                            replacement = r[i + 10], 
+                            x = grep(p[i + 10], df23$exp, value = TRUE)) %>% 
                 paste("-", ., sep = "")
         }
 
@@ -257,15 +267,18 @@ df23 <- df23 %>% spread(key = "item", value = "exp")
 df23 <- df23 %>% select( - one_of("000"))
 
 ##### items with no observations #####
-# df23 (626 variables) but all items are 800
 # names of all the items
-colnames(df23)[-1] <- colnames(df23)[-1] %>% as.integer() %>% paste("itm", ., sep = "")
-itms_all <- doc.items.part1 %>% as.integer() %>% paste("itm" , ., sep = "")
+colnames(df23)[-1] <- colnames(df23)[-1] %>% 
+        as.integer() %>% 
+        paste("itm", ., sep = "")
+itms_all <- doc.items.digits %>% 
+        as.integer() %>% 
+        paste("itm" , ., sep = "")
 # create a tibble for those who are not in df23
 df.itm.all <- matrix("NA", nrow = nrow(df23), ncol = length(itms_all)) %>% 
         as_tibble(.name_repair = NULL)
 # create x1 column for merging
-df.itm.all$`x1` <- df23$`x1`
+df.itm.all$x1 <- df23$x1
 # name the columns with all item names
 colnames(df.itm.all) <- c(itms_all, "x1")
 # merge
@@ -273,24 +286,33 @@ df23 <- df23 %>% left_join(df.itm.all) %>%
         # column types (hablar::convert)
         convert(chr(x1), num(contains("itm"))) %>% 
         # order
-        .[order(.$`x1`), ]
+        .[order(.$x1), ]
 # free up ram
 gc()
 
 ##### merge #####
 data.list <- list(df1, df2, df21, df22, df23)
-df.inc89 <- Reduce(function(...) left_join(..., by = "x1"), data.list)
+df.inc <- Reduce(function(...) left_join(..., by = "x1"), data.list)
 # add year column
-df.inc89$year <- as.integer(89)
+df.inc$year <- as.integer(year)
+#
+df.inc90 <- df.inc
+code_tbl_90 <- code_tbl
 # remove
-rm(df.source, x, df.itm.all, df1, df2, df21, df22, df23, data.list)
+rm(df.source, x, df.itm.all, 
+   df1, df2, df21, df22, df23, 
+   data.list, df.inc, code_tbl)
 # free up memory
 gc()
 
 ##### save ###### 
 # .RData
-save(df.inc89, file = "AA170025/df_inc89.RData")
-save(code_tbl_89, file = "AA170025/code_tbl_89.RData")
+# save df.inc
+save(df.inc90, file = "AA170026/df_inc90.RData")
+save(df.inc90, file = "R data files/df_inc90.RData")
+# save code_tbl
+save(code_tbl_90, file = "AA170026/code_tbl_90.RData")
+save(code_tbl_90, file = "R data files/code_tbl_90.RData")
 # .csv format
 # write_csv(df.inc106, "inc106.csv", col_names = TRUE, na = "")
 # .sas7bdat format
