e391f1edc27d925b8885e9dd575dd561dfccb7cb
CaiYunTing <caiyuntingcfrc@gmail.com>
2019-10-09 17:53:06 +0800

    1009_

--- 1009pvertyRate.R
+++ 1009pvertyRate.R
@@ -1,16 +1,17 @@
 rm(list = ls())
-source("~/Github_CFRC/misc/func_PovertyRate.R")
+source("~/github/misc/func_PovertyRate.R")
 
+setwd("~/R_wd/tw_inc/")
 
 # load Rdata --------------------------------------------------------------
 
 load("AA170025/inc89.RData")
 load("AA170030/inc94.RData")
 load("AA170035/inc99.RData")
+load("AA170036/inc100.RData")
 load("AA170040/inc104.RData")
 load("AA170042/inc106.RData")
 load("AA170043/inc107.RData")
-
 
 # poverty rates -----------------------------------------------------------
 
@@ -19,6 +20,8 @@ poverty_rate(df = df.inc89, weight = "a21", year = 89) %>%
 poverty_rate(df = df.inc94, weight = "a20", year = 94) %>% 
         round(digits = 2)
 poverty_rate(df = df.inc99, weight = "a20", year = 99) %>% 
+        round(digits = 2)
+poverty_rate(df = df.inc100, weight = "a20", year = 100) %>% 
         round(digits = 2)
 poverty_rate(df = df.inc104, weight = "a20", year = 104) %>% 
         round(digits = 2)
